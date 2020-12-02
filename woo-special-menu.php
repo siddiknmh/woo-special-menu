018a77ee42bcd0a4faea90564f75b19911992296
AB Siddik <siddiknmh@gmail.com>
2020-12-02 21:40:35 +0600

    add plugin header

---
+++ woo-special-menu.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Plugin Name:       Woo Special Menu
+* Plugin URI:        https://github.com/siddiknmh/woo-special-menu
+* Description:       WooCommerce special menu for users. Which will show depends on the different type of condition set by shop owner.
+* Version:           1.0
+* Requires at least: 5.0
+* Requires PHP:      7.0
+* Author:            AB Siddik
+* Author URI:        https://github.com/siddiknmh/
+* License:           GPL v2 or later
+* License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain:       woo-special-menu
+* Domain Path:       /languages
+*/
